edbc4b88d241031cbb532b54917eae2b52b68627
SleepLighter <zekamikhailov96@gmail.com>
2024-10-08 16:19:43 +0300

    Add files via upload

---
+++ Uhh2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uhh2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Translator_click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, string> translations = new Dictionary<string, string>
+            {
+            { "солнце", "sun" },
+            { "дождь", "rain" },
+            { "снег", "snow" },
+            { "ветер", "wind" },
+            { "облако", "cloud" },
+            { "туман", "fog" },
+            { "град", "hail" },
+            { "мороз", "frost" },
+            { "гром", "thunder" },
+            { "молния", "lightning" }
+        };
+
+            Console.WriteLine("Введите слово на русском языке:");
+            string input = Console.ReadLine();
+
+            if (translations.ContainsKey(input))
+            {
+                Console.WriteLine("Перевод: " + translations[input]);
+            }
+            else
+            {
+                Console.WriteLine("Такого слова нет.");
+            }
+        }
+    }
+}
---
+++ Uhh2/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Uhh2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    
+    {
+        Dictionary<string, string> translations = new Dictionary<string, string>
+            {
+            { "солнце", "sun" },
+            { "дождь", "rain" },
+            { "снег", "snow" },
+            { "ветер", "wind" },
+            { "облако", "cloud" },
+            { "туман", "fog" },
+            { "град", "hail" },
+            { "мороз", "frost" },
+            { "гром", "thunder" },
+            { "молния", "lightning" }
+        };
+
+        Console.WriteLine("Введите слово на русском языке:");
+        string input = Console.ReadLine();
+
+        if (translations.ContainsKey(input))
+        {
+            Console.WriteLine("Перевод: " + translations[input]);
+        }
+        else
+        {
+            Console.WriteLine("Такого слова нет.");
+        }
+}
